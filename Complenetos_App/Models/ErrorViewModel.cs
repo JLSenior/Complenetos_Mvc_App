f763dcd686f9981f93c9152803285cbff5986802
Juan Lopez <jlsenior19@gmail.com>
2022-08-14 12:35:04 +0200

    Agregar archivos de proyecto.

---
+++ Complenetos_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Complenetos_App.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
